ca34fd06c0ea0e001c550544cfd584c450c9fc7c
Keshitan <toshiki_6631@yahoo.co.jp>
2023-01-03 08:49:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Tuple, Record } = require("./src/record-tuple")
 
-const tuple = Tuple(0, 1, 2, 3);
-const record = Record({ foo: "bar" });
-console.log(tuple, record);
+const tuple = Tuple(0, 1, 2, 3)
+const record = Record({ foo: "bar" })
+console.log(tuple, record)
 //=> #[ 0, 1, 2, 3 ] in Node.js v18.12.1
